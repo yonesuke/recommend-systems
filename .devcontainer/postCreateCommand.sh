d00a6c303226499019d1d9ab6fb04f731a667bd8
Ryosuke Yoneda <13e.e.c.13@gmail.com>
2024-03-21 08:03:40 +0000

    add download command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,4 @@
 pip3 install --upgrade pip
 pip3 install --user -r requirements.txt
+wget -nc --no-check-certificate https://files.grouplens.org/datasets/movielens/ml-10m.zip -P data
+unzip data/ml-10m.zip -d data/
